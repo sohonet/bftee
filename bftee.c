a197c39e88fdff53ccb76e361af2df57ccffe50f
Ben Roeder <ben.roeder@sohonet.com>
2025-07-09 12:51:01 +0100

    Standardize all comments to C-style /* */ format
    - Convert all // comments to /* */ for consistency
    - Maintain original comment content and formatting
    - Align with C coding conventions

--- bftee.c
+++ bftee.c
@@ -13,7 +13,7 @@
     #include <signal.h>
     #include <unistd.h>
 
-    // the number of sBuffers that are being held at a maximum
+    /* the number of sBuffers that are being held at a maximum */
     #define BUFFER_SIZE 4096
     #define BLOCK_SIZE 2048
 
@@ -23,29 +23,29 @@
     } sBuffer;
 
     typedef struct {
-      sBuffer *data;  //array of buffers
-      int bufferSize; // number of buffer in data
-      int start;      // index of the current start buffer
-      int end;        // index of the current end buffer
-      int active;     // number of active buffer (currently in use)
-      int maxUse;     // maximum number of buffers ever used
-      int drops;      // number of discarded buffer due to overflow
-      int sWrites;    // number of buffer written to stdout
-      int pWrites;    // number of buffers written to pipe
+      sBuffer *data;  /* array of buffers */
+      int bufferSize; /* number of buffer in data */
+      int start;      /* index of the current start buffer */
+      int end;        /* index of the current end buffer */
+      int active;     /* number of active buffer (currently in use) */
+      int maxUse;     /* maximum number of buffers ever used */
+      int drops;      /* number of discarded buffer due to overflow */
+      int sWrites;    /* number of buffer written to stdout */
+      int pWrites;    /* number of buffers written to pipe */
     } sQueue;
 
-    void InitQueue(sQueue*, int);              // initialized the Queue
-    void PushToQueue(sQueue*, sBuffer*, int);  // pushes a buffer into Queue at the end 
-    sBuffer *RetrieveFromQueue(sQueue*);       // returns the first entry of the buffer and removes it or NULL is buffer is empty
-    sBuffer *PeakAtQueue(sQueue*);             // returns the first entry of the buffer but does not remove it. Returns NULL on an empty buffer
-    void ShrinkInQueue(sQueue *queue, int);    // shrinks the first entry of the buffer by n-bytes. Buffer is removed if it is empty
-    void DelFromQueue(sQueue *queue);          // removes the first entry of the queue
+    void InitQueue(sQueue*, int);              /* initialized the Queue */
+    void PushToQueue(sQueue*, sBuffer*, int);  /* pushes a buffer into Queue at the end */
+    sBuffer *RetrieveFromQueue(sQueue*);       /* returns the first entry of the buffer and removes it or NULL is buffer is empty */
+    sBuffer *PeakAtQueue(sQueue*);             /* returns the first entry of the buffer but does not remove it. Returns NULL on an empty buffer */
+    void ShrinkInQueue(sQueue *queue, int);    /* shrinks the first entry of the buffer by n-bytes. Buffer is removed if it is empty */
+    void DelFromQueue(sQueue *queue);          /* removes the first entry of the queue */
 
-    static void sigUSR1(int);                  // signal handled for SUGUSR1 - used for stats output to stderr
-    static void sigINT(int);                   // signla handler for SIGKILL/SIGTERM - allows for a graceful stop ?
+    static void sigUSR1(int);                  /* signal handled for SUGUSR1 - used for stats output to stderr */
+    static void sigINT(int);                   /* signla handler for SIGKILL/SIGTERM - allows for a graceful stop ? */
 
-    sQueue queue;                              // Buffer storing the overflow
-    volatile int quit;                         // for quiting the main loop
+    sQueue queue;                              /* Buffer storing the overflow */
+    volatile int quit;                         /* for quiting the main loop */
 
     int main(int argc, char *argv[])
     {   
@@ -114,18 +114,18 @@
 
         while(!quit)
         {
-            // read from STDIN
+            /* read from STDIN */
             bytes = read(STDIN_FILENO, buffer.data, sizeof(buffer.data));
 
-            // if read failed due to interrupt, then retry, otherwise STDIN has closed and we should stop reading
+            /* if read failed due to interrupt, then retry, otherwise STDIN has closed and we should stop reading */
             if (bytes < 0 && errno == EINTR) continue;
             if (bytes <= 0) break;
 
-            // save the number if read bytes in the current buffer to be processed
+            /* save the number if read bytes in the current buffer to be processed */
             buffer.bytes = bytes;
 
-            // this is a blocking write. As long as buffer is smaller than 4096 Bytes, the write is atomic to a pipe in Linux
-            // thus, this cannot be interrupted. however, to be save this should handle the error cases of partial or interrupted write none the less.
+            /* this is a blocking write. As long as buffer is smaller than 4096 Bytes, the write is atomic to a pipe in Linux
+               thus, this cannot be interrupted. however, to be save this should handle the error cases of partial or interrupted write none the less. */
             bytes = write(STDOUT_FILENO, buffer.data, buffer.bytes);
             queue.sWrites++;
 
@@ -136,49 +136,49 @@
 
             sBuffer *tmpBuffer = NULL;
 
-            // if the queue is empty (tmpBuffer gets set to NULL) the this does nothing - otherwise it tries to write
-            // the buffered data to the pipe. This continues until the Buffer is empty or the write fails.
-            // NOTE: bytes cannot be -1  (that would have failed just before) when the loop is entered. 
+            /* if the queue is empty (tmpBuffer gets set to NULL) the this does nothing - otherwise it tries to write
+               the buffered data to the pipe. This continues until the Buffer is empty or the write fails.
+               NOTE: bytes cannot be -1  (that would have failed just before) when the loop is entered. */
             while ((bytes != -1) && (tmpBuffer = PeakAtQueue(&queue)) != NULL) {
-               // write the oldest buffer to the pipe
+               /* write the oldest buffer to the pipe */
                bytes = write(writefd, tmpBuffer->data, tmpBuffer->bytes);
 
-               // the  written bytes are equal to the buffer size, the write is successful - remove the buffer and continue
+               /* the  written bytes are equal to the buffer size, the write is successful - remove the buffer and continue */
                if (bytes == tmpBuffer->bytes) {
                  DelFromQueue(&queue);
                  queue.pWrites++;
                } else if (bytes > 0) {
-                 // on a positive bytes value there was a partial write. we shrink the current buffer
-                 //  and handle this as a write failure
+                 /* on a positive bytes value there was a partial write. we shrink the current buffer
+                    and handle this as a write failure */
                  ShrinkInQueue(&queue, bytes);
                  bytes = -1;
                }
             }
-            // There are several cases here:
-            // 1.) The Queue is empty -> bytes is still set from the write to STDOUT. in this case, we try to write the read data directly to the pipe
-            // 2.) The Queue was not empty but is now -> bytes is set from the last write (which was successful) and is bigger 0. also try to write the data
-            // 3.) The Queue was not empty and still is not -> there was a write error before (even partial), and bytes is -1. Thus this line is skipped.
+            /* There are several cases here:
+               1.) The Queue is empty -> bytes is still set from the write to STDOUT. in this case, we try to write the read data directly to the pipe
+               2.) The Queue was not empty but is now -> bytes is set from the last write (which was successful) and is bigger 0. also try to write the data
+               3.) The Queue was not empty and still is not -> there was a write error before (even partial), and bytes is -1. Thus this line is skipped. */
             if (bytes != -1) bytes = write(writefd, buffer.data, buffer.bytes);
 
-            // again, there are several cases what can happen here
-            // 1.) the write before was successful -> in this case bytes is equal to buffer.bytes and nothing happens
-            // 2.) the write just before is partial or failed all together - bytes is either -1 or smaller than buffer.bytes -> add the remaining data to the queue
-            // 3.) the write before did not happen as the buffer flush already had an error. In this case bytes is -1 -> add the remaining data to the queue
+            /* again, there are several cases what can happen here
+               1.) the write before was successful -> in this case bytes is equal to buffer.bytes and nothing happens
+               2.) the write just before is partial or failed all together - bytes is either -1 or smaller than buffer.bytes -> add the remaining data to the queue
+               3.) the write before did not happen as the buffer flush already had an error. In this case bytes is -1 -> add the remaining data to the queue */
             if (bytes != buffer.bytes)
               PushToQueue(&queue, &buffer, bytes);
             else 
               queue.pWrites++;
         }
 
-        // once we are done with STDIN, try to flush the buffer to the named pipe
+        /* once we are done with STDIN, try to flush the buffer to the named pipe */
         if (queue.active > 0) {
-           // set output buffer to blocking mode to ensure all buffered data is written
+           /* set output buffer to blocking mode to ensure all buffered data is written */
            int saved_flags = fcntl(writefd, F_GETFL);
            int new_flags = saved_flags & ~O_NONBLOCK;
            fcntl(writefd, F_SETFL, new_flags);
 
            sBuffer *tmpBuffer = NULL;
-           //TODO: this does not handle partial writes yet
+           /* TODO: this does not handle partial writes yet */
            while ((tmpBuffer = PeakAtQueue(&queue)) != NULL) {
              int bytes = write(writefd, tmpBuffer->data, tmpBuffer->bytes);
              if (bytes != -1) DelFromQueue(&queue);
@@ -207,29 +207,29 @@
     void PushToQueue(sQueue *queue, sBuffer *p, int offset)
     {
 
-        if (offset < 0) offset = 0;      // offset cannot be smaller than 0 - if that is the case, we were given an error code. Set it to 0 instead
-        if (offset == p->bytes) return;  // in this case there are 0 bytes to add to the queue. Nothing to write
+        if (offset < 0) offset = 0;      /* offset cannot be smaller than 0 - if that is the case, we were given an error code. Set it to 0 instead */
+        if (offset == p->bytes) return;  /* in this case there are 0 bytes to add to the queue. Nothing to write */
 
-        // this should never happen - offset cannot be bigger than the buffer itself. Panic action
+        /* this should never happen - offset cannot be bigger than the buffer itself. Panic action */
         if (offset > p->bytes) {perror("got more bytes to buffer than we read\n"); exit(EXIT_FAILURE);}
 
-        // debug output on a partial write. TODO: remove this line
-        // if (offset > 0 ) fprintf(stderr, "partial write to buffer\n");
+        /* debug output on a partial write. TODO: remove this line
+           if (offset > 0 ) fprintf(stderr, "partial write to buffer\n"); */
 
-        // copy the data from the buffer into the queue and remember its size
+        /* copy the data from the buffer into the queue and remember its size */
         memcpy(queue->data[queue->end].data, p->data + offset , p->bytes-offset);
         queue->data[queue->end].bytes = p->bytes - offset;
 
-        // move the buffer forward
+        /* move the buffer forward */
         queue->end = (queue->end + 1) % queue->bufferSize;
 
-        // there is still space in the buffer
+        /* there is still space in the buffer */
         if (queue->active < queue->bufferSize)
         {
             queue->active++;
             if (queue->active > queue->maxUse) queue->maxUse = queue->active;
         } else {
-            // Overwriting the oldest. Move start to next-oldest
+            /* Overwriting the oldest. Move start to next-oldest */
             queue->start = (queue->start + 1) % queue->bufferSize;
             queue->drops++;
         }
@@ -255,24 +255,24 @@
     /*** Shrinks the oldest entry i the Queue by bytes. Removes the entry if buffer of the oldest entry runs empty*/
     void ShrinkInQueue(sQueue *queue, int bytes) {
 
-      // cannot remove negative amount of bytes - this is an error case. Ignore it
+      /* cannot remove negative amount of bytes - this is an error case. Ignore it */
       if (bytes <= 0) return;
 
-      // remove the entry if the offset is equal to the buffer size
+      /* remove the entry if the offset is equal to the buffer size */
       if (queue->data[queue->start].bytes == bytes) {
         DelFromQueue(queue);
         return;
       };
 
-      // this is a partial delete
+      /* this is a partial delete */
       if (queue->data[queue->start].bytes > bytes) {
-        //shift the memory by the offset
+        /* shift the memory by the offset */
         memmove(queue->data[queue->start].data, queue->data[queue->start].data + bytes, queue->data[queue->start].bytes - bytes);
         queue->data[queue->start].bytes = queue->data[queue->start].bytes - bytes;
         return;
       }
 
-      // panic is the are to remove more than we have the buffer
+      /* panic is the are to remove more than we have the buffer */
       if (queue->data[queue->start].bytes < bytes) {
         perror("we wrote more than we had - this should never happen\n");
         exit(EXIT_FAILURE);
